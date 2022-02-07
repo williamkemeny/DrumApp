a241064f11781ce20e7037a8bc84d05001a1e6bc
William Kemeny <williamkemeny@gmail.com>
2022-02-07 13:45:03 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Howl } from "howler";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = (props) => {
+  console.log(props.nameBank);
+  const [displayName, setDisplayName] = useState(props.nameBank);
+  const [volumeControl, setVolumeControl] = useState(0.5);
+
+  useEffect(() => {
+    setDisplayName(props.nameBank);
+  }, [props]);
+
+  const soundPlay = (src) => {
+    const sound = new Howl({
+      src,
+      html5: true,
+      volume: volumeControl,
+    });
+    sound.play();
+  };
+
+  const handleKeydown = (e) => {
+    if (document.getElementById(e.key.toUpperCase()) !== null) {
+      document.getElementById(e.key.toUpperCase()).click();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeydown);
+  }, []);
+
+  const RenderButtonSound = () => {
+    return props.sound.map((soundObj) => {
+      return (
+        <button
+          key={soundObj.keyCode}
+          id={soundObj.keyTrigger}
+          onClick={() => {
+            soundPlay(soundObj.url);
+            setDisplayName(soundObj.id);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {soundObj.keyTrigger}
+        </button>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <div className="Drum">
+        {RenderButtonSound()}
+        <h3 id="display">{displayName}</h3>
+      </div>
+      <div className="slidecontainer">
+        <input
+          type="range"
+          min={0}
+          max={1}
+          step={0.02}
+          value={volumeControl}
+          onChange={(event) => {
+            setVolumeControl(event.target.valueAsNumber);
+          }}
+        />
+      </div>
     </div>
   );
-}
-
+};
 export default App;
